81526ba109079d1e5c85a3f517a2155b04071ecc
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-21 18:23:40 +0300

    setp up react app

---
+++ recipe/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import './components/SearchBar';
+import './components/RecipeCard'
+
+function App() {
+  return (
+    <div className="container">
+      
+    </div>
+  );
+}
+
+export default App;
