d3ddfd1b33686e04a504e4d802214cc19134bb9d
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-23 19:42:12 +0300

    add favorite page

---
+++ recipe/src/pages/favorite-page/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useEffect } from "react";
+import './style.css'
+
+const FavotirePage = ()=>{
+    
+    return(
+        <div></div>
+    )
+}
+
+export default FavotirePage;
